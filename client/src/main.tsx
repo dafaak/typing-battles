f456e7132a9ad6b35eaffff3802ae18f296320df
Israel <israel.pe@outlook.es>
2025-02-14 11:33:48 -0500

    feat: socket context

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { SocketProvider } from './context/SocketContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SocketProvider>
+    <Router>
+      <App />
+    </Router>
+    </SocketProvider>
   </StrictMode>,
 )
