127a40ae55e8192190f34c3d8449e32b9c21d41f
Israel <israel.pe@outlook.es>
2025-02-19 20:14:22 -0500

    feat: show  current player name

--- client/src/pages/Loby.tsx
+++ client/src/pages/Loby.tsx
@@ -9,11 +9,13 @@ export function Loby() {
     const socket = useSocket();
     const dispatch = useDispatch();
     const players = useSelector((state: RootState) => state.players.players);
+    const player = useSelector((state: RootState) => state.playerData.player);
 
 
     return (
         <>
             <h1>Loby</h1>
+            <h3>Hi, {player && player.name} {player && player.conn_id}</h3>
             <div>
                 {players.map((player) => (
                     <div key={player.conn_id}>
