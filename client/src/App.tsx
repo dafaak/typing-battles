651e58aff094065b8b3f6619989d8fc18deb551e
dafaak <23407589+dafaak@users.noreply.github.com>
2025-12-29 13:28:22 -0500

    feat: update game state handling and routing in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,20 +6,34 @@ import { useEffect } from "react";
 import { GameStatus } from "./interfaces/GameStatus.ts";
 import { setPlayers } from "./redux/playersSlice.ts";
 import { useSocket } from "./context/SocketContext.tsx";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Player } from "./interfaces/Player.ts";
 import { setPlayerData } from "./redux/playerDataSlice.ts";
+import { setPartie, setText } from "./redux/partieSlice.ts";
+import { Game } from "./pages/Game.tsx";
+import { RootState } from "./redux/store.ts";
 
 function App() {
   const navigate = useNavigate();
   const socket = useSocket();
   const dispatch = useDispatch();
+  const player = useSelector((state: RootState) => state.playerData.player);
 
   useEffect(() => {
-    function onPlayersUpdate(data: string) {
+    function onGameUpdate(data: string) {
       const gameStatus: GameStatus = JSON.parse(data);
-      console.log(gameStatus);
+      dispatch(setPartie({state: gameStatus.state, name: gameStatus.name}));
       dispatch(setPlayers(gameStatus.players));
+
+
+
+      if (gameStatus.state === 'ready') {
+        dispatch(setText(gameStatus.targetString?.trim()))
+      }
+
+      if (gameStatus.state === 'running') {
+        navigate('/game');
+      }
     }
 
     function onJoinSuccess(data: string) {
@@ -30,10 +44,10 @@ function App() {
     }
 
     socket.on('join-room-success', onJoinSuccess);
-    socket.on('players-update', onPlayersUpdate);
+    socket.on('game-update', onGameUpdate);
 
     return () => {
-      socket.off('players-update', onPlayersUpdate);
+      socket.off('players-update', onGameUpdate);
       socket.off('join-room-success', onJoinSuccess);
     };
 
@@ -43,6 +57,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/loby" element={<Loby/>}/>
+        <Route path="/game" element={<Game/>}/>
       </Routes>
   )
 }
